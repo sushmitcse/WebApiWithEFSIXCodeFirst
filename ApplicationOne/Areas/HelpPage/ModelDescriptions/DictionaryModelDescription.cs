b15c318d1bef467061b6c9c2f68f703adc305ec8
Goswami <sushmit.goswami@philips.com>
2018-02-15 17:38:23 +0530

    First commit

---
+++ ApplicationOne/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ApplicationOne.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ApplicationOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApplicationOne.WebApiApplication" Language="C#" %>
